59e167588297f22669de9c0c242be42ac06e9f55
SandunBuddhika <sandunbuddika09@gmail.com>
2024-04-24 15:38:40 +0530

    Full Project

---
+++ src/main/java/com/sandun/app/Runner.java
@@ -0,0 +1,10 @@
+package com.sandun.app;
+
+import javafx.application.Application;
+
+public class Runner  {
+
+    public static void main(String[] args) {
+        Application.launch(ParticleSimulation.class, args);
+    }
+}
