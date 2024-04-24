ace4357fd2fbb317c15aedf5e0406a658e83ffaf
SandunBuddhika <sandunbuddika09@gmail.com>
2024-04-24 15:30:16 +0530

    Revert "Full Project"
    This reverts commit f7fe27e33338eba668b5e1744f5c3cdccc9daf88.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.sandun.app.javafx_particlesimulation {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.sandun.app to javafx.fxml;
-    exports com.sandun.app;
-}
